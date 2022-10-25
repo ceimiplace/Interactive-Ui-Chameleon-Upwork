12ab35373daf0d49d8ec2a1c6dcc0d90e03221e5
ceimiplace <100032730+ceimiplace@users.noreply.github.com>
2022-10-25 15:11:26 +0300

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+let navs = document.querySelectorAll(".nav-button");
+let navigation = document.querySelector(".navigation");
+navs.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    navs.forEach((elem2) => {
+      elem2.classList.remove("nav-button--active");
+    });
+    elem.classList.add("nav-button--active");
+  });
+});
+let logo = document.querySelector(".logo");
+logo.addEventListener("click", () => {
+  logo.classList.add("spin");
+  let width = document.body.clientWidth;
+  console.log(width);
+  if (width < 768) {
+    navigation.classList.toggle("collapse");
+  }
+});
+logo.addEventListener("animationend", () => {
+  logo.classList.remove("spin");
+});
